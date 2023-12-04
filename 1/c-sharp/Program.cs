45cf0692a7df2a77682d377c23cea86ff7d253c2
Malcolm <mhannah@softwareteclab.co.uk>
2023-12-04 18:11:42 +0000

    Start C sharp version

---
+++ 1/c-sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Calibration
+{
+    private string _line;
+
+    public Calibration(string line)
+    {
+        _line = line;
+    }
+
+    private int FindFirst(int digit)
+    {
+        return 0;
+    }
+}
